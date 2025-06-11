3b26df50192390fd7ee13e0a96c5d2381c75fe65
Prajakta <prajaktamedhekar2001@gmail.com>
2025-06-11 14:19:39 +0530

    all folder file created

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
 let express=require('express');
 let app=express();
 
+const router=require("./routes/regRoutes.js");
+let db=require("./config/db.js");
+let cookie=require('cookie-parser');
+let session=require("express-session");
+
+let bodyparser=require('body-parser');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.set('view engine',"ejs");
+
+
+app.use("/",router);
+
+
 module.exports=app;
+
